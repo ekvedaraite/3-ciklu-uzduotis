7d73c70ea7417bf68cb59d6ab4ecbb62b8a79f59
ekvedaraite <evelinakvedaraite2003@gmail.com>
2023-11-17 13:44:04 +0200

    init

---
+++ script.js
@@ -0,0 +1,24 @@
+// 1 užduotis
+for (let i = 0; i < 10; i++) {
+    console.log('Evelina');
+  }
+
+// 2 užduotis
+var vardas = "Evelina";
+var kartai = 10;
+
+for (let i = 0; i < kartai; i++) {
+    console.log(vardas);
+}
+
+// 3 užduotis
+var vardas = "Evelina";
+var kartai = 10;
+
+for (let i = 0; i < kartai; i++) {
+    console.log(i + '. ' + vardas);
+}
+
+for (let i = 10; i > 0; i--) {
+    console.log(i + '. ' + vardas);
+}
